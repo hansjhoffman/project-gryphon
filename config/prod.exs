1356e493e523d57d6d34a9170222dbbe6ac7f917
Hans Hoffman <hoffman.hans.j@gmail.com>
2020-08-11 18:25:25 -0400

    chore: massage configs for gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -10,12 +10,15 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :gryphon, GryphonWeb.Endpoint,
-  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  force_ssl: [hsts: true],
-  ssl: true
+  force_ssl: [
+    rewrite_on: [
+      :x_forwarded_proto
+    ]
+  ],
+  server: true,
+  ssl: true,
+  url: [host: nil, port: 443]
 
 # Do not print debug messages in production
 config :logger, level: :info
-
-import_config "prod.secret.exs"
--- config/releases.exs
+++ config/releases.exs
@@ -1,5 +1,19 @@
 import Config
 
+app_name =
+  System.get_env("APP_NAME") ||
+    raise """
+    environment variable APP_NAME is missing.
+    """
+
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
 config :gryphon, GryphonWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT", "4000") |> String.to_integer()],
-  url: [host: nil, port: 443]
+  url: [host: app_name <> ".gigalixirapp.com", port: 443],
+  secret_key_base: secret_key_base
