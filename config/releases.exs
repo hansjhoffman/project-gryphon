759a569820563ea26cd91bc674a02f43789b4dea
Hans Hoffman <hoffman.hans.j@gmail.com>
2020-08-11 16:36:24 -0400

    chore: change to port 443

--- config/releases.exs
+++ config/releases.exs
@@ -2,4 +2,4 @@ import Config
 
 config :gryphon, GryphonWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT", "4000") |> String.to_integer()],
-  url: [host: nil, port: System.get_env("PORT")]
+  url: [host: nil, port: 443]
